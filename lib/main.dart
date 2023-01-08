b0a41f1d6d693afe66a3227b26b3a1c1f8fd1de7
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-01-09 00:38:30 +0900

    不要なファイルや記述の削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 
-import './providers/paint_provider.dart';
-import './models/home_model.dart';
 import './screens/home_screen.dart';
 import './screens/health_condition.dart';
 import './screens/paint_screen.dart';
@@ -26,35 +23,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => PaintProvider(),
+    return MaterialApp(
+      title: 'Text To Speech Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.redAccent[100],
+          secondary: Colors.greenAccent,
+          background: Colors.greenAccent[100],
+          error: Colors.red,
         ),
-        ChangeNotifierProvider(
-          create: (context) => HomeModel(),
-        )
-      ],
-      child: MaterialApp(
-          title: 'Text To Speech Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch().copyWith(
-              primary: Colors.redAccent[100],
-              secondary: Colors.greenAccent,
-              background: Colors.greenAccent[100],
-              error: Colors.red,
-            ),
-            buttonTheme: const ButtonThemeData(
-              buttonColor: Colors.amber,
-            ),
-          ),
-          home: const TitleScreen(),
-          routes: {
-            HomeScreen.routeName: (context) => HomeScreen(),
-            HealthCondition.routeName: (context) => const HealthCondition(),
-            PaintScreen.routeName: (context) => const PaintScreen(),
-            TakeHand.routeName: (context) => const TakeHand(),
-          }),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Colors.amber,
+        ),
+      ),
+      home: const TitleScreen(),
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        HealthCondition.routeName: (context) => const HealthCondition(),
+        PaintScreen.routeName: (context) => const PaintScreen(),
+        TakeHand.routeName: (context) => const TakeHand(),
+      },
     );
   }
 }
