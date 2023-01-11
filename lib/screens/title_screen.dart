18e376e79bac92d4a022a0c053a13549aede8e75
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-01-11 11:29:33 +0900

    titleからのページ遷移を変更

--- lib/screens/title_screen.dart
+++ lib/screens/title_screen.dart
@@ -10,85 +10,95 @@ class TitleScreen extends StatefulWidget {
 }
 
 class _TitleScreenState extends State<TitleScreen> {
+  Future<void> pageTransition() async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    Navigator.of(context).pushReplacement(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return const HomeScreen();
+        },
+        transitionDuration: const Duration(seconds: 1),
+        reverseTransitionDuration: const Duration(seconds: 1),
+        transitionsBuilder: ((context, animation, secondaryAnimation, child) {
+          final color = ColorTween(
+            begin: Colors.transparent,
+            end: Colors.black,
+          ).animate(CurvedAnimation(
+            parent: animation,
+            curve: const Interval(
+              0.0,
+              0.5,
+              curve: Curves.easeInOut,
+            ),
+          ));
+          final opacity = Tween<double>(
+            begin: 0.0,
+            end: 1.0,
+          ).animate(CurvedAnimation(
+            parent: animation,
+            curve: const Interval(
+              0.5,
+              1.0,
+              curve: Curves.easeInOut,
+            ),
+          ));
+          return AnimatedBuilder(
+            animation: animation,
+            builder: (context, child) {
+              return Container(
+                color: color.value,
+                child: Opacity(
+                  opacity: opacity.value,
+                  child: child,
+                ),
+              );
+            },
+            child: child,
+          );
+        }),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pageTransition();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.of(context).pushReplacement(
-        PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) {
-            return const HomeScreen();
-          },
-          transitionDuration: const Duration(seconds: 1),
-          reverseTransitionDuration: const Duration(seconds: 1),
-          transitionsBuilder: ((context, animation, secondaryAnimation, child) {
-            final color = ColorTween(
-              begin: Colors.transparent,
-              end: Colors.black,
-            ).animate(CurvedAnimation(
-              parent: animation,
-              curve: const Interval(
-                0.0,
-                0.5,
-                curve: Curves.easeInOut,
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              '伝えたい気持ちをサポートする',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '音声会話補助アプリ',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'comecome',
+              style: TextStyle(
+                fontSize: 42,
+                fontWeight: FontWeight.w900,
+                fontStyle: FontStyle.italic,
               ),
-            ));
-            final opacity = Tween<double>(
-              begin: 0.0,
-              end: 1.0,
-            ).animate(CurvedAnimation(
-              parent: animation,
-              curve: const Interval(
-                0.5,
-                1.0,
-                curve: Curves.easeInOut,
-              ),
-            ));
-            return AnimatedBuilder(
-              animation: animation,
-              builder: (context, child) {
-                return Container(
-                  color: color.value,
-                  child: Opacity(
-                    opacity: opacity.value,
-                    child: child,
-                  ),
-                );
-              },
-              child: child,
-            );
-          }),
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                '伝えたい気持ちをサポートする',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                '音声会話補助アプリ',
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'comecome',
-                style: TextStyle(
-                  fontSize: 42,
-                  fontWeight: FontWeight.w900,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
