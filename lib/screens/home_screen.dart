8d0d4f8dbd678c5740e16e44ae91f763d12b1423
Shun <shunsuke.takasu.job@gmail.com>
2023-01-13 19:23:28 +0900

    Edit: Split home screen into top_Bar file

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:provider/provider.dart';
 import 'package:text_to_speech_demo/db/sqlCrud.dart';
+import 'package:text_to_speech_demo/widgets/top_Bar.dart';
 
 import '../widgets/create_Dialog.dart';
 import './health_condition.dart';
@@ -43,7 +44,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> _refreshJournals() async {
     final data = await SqlCrud.getItems();
     if (data.isEmpty) {
-      print("empty");
       await SqlCrud.createItem(title: "ありがとう", descrption: "ありがとうございます");
       await SqlCrud.createItem(title: "すいません", descrption: "すいません");
       await SqlCrud.createItem(title: "のどがかわきました", descrption: "のどが､かわきました");
@@ -51,8 +51,6 @@ class _HomeScreenState extends State<HomeScreen> {
       await SqlCrud.createItem(title: "トイレ", descrption: "トイレに行きたいです");
       await SqlCrud.createItem(title: "ぐあいがわるいです", descrption: "具合が悪いです");
       _refreshJournals();
-    } else {
-      print("notEmpty");
     }
     setState(() {
       _journals = data;
@@ -144,60 +142,7 @@ class _HomeScreenState extends State<HomeScreen> {
           return Scaffold(
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
-              title: Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 50,
-                      child: Form(
-                        child: TextFormField(
-                          controller: _controller,
-                          decoration: const InputDecoration(
-                            labelText: 'お名前入力',
-                            fillColor: Colors.white,
-                            filled: true,
-                          ),
-                          onChanged: ((value) {
-                            setState(() {
-                              name = value;
-                            });
-                          }),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Text(
-                    'さん来てください',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      name.isEmpty
-                          ? _speak("誰か来てください")
-                          : _speak('$nameさん来てください');
-                    },
-                    child: Column(
-                      children: [
-                        const Icon(
-                          Icons.screen_rotation,
-                          color: Colors.black,
-                        ),
-                        Text(
-                          'スマホを振ってください',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Colors.redAccent[700],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+              title: const TopBar(),
             ),
             body: _isLoading
                 ? const Center(
