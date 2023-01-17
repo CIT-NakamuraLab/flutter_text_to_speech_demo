5b105bb1ece5d53aa92590b9836dfd5187bbdae5
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-01-17 17:28:51 +0900

    HomeScreenとSpeechToTextのFloatingButtonを長押しで移動可能に変更

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:text_to_speech_demo/db/sqlCrud.dart';
@@ -26,8 +27,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Map<String, dynamic>> _journals = [];
   bool _isLoading = true;
+  bool _isCalled = false;
 
   final category = HomeScreen.routeName.toString().replaceAll("/", "");
+
+  Offset position = const Offset(0, 0);
+
   // データを引っ張る
   Future<void> refreshJournals() async {
     final data = await SqlCrud.refreshAndInitJournals(category: category);
@@ -59,6 +64,10 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  void updatePosition(BuildContext context) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     // 追加するI/Oになる際には､reload回数が52回まで増加
@@ -67,6 +76,13 @@ class _HomeScreenState extends State<HomeScreen> {
         AppBar().preferredSize.height -
         MediaQuery.of(context).padding.top;
     final deviceWidth = MediaQuery.of(context).size.width;
+    setState(() {
+      if (!_isCalled) {
+        position = Offset(deviceWidth * 0.8, deviceHeight * 0.75);
+      }
+      _isCalled = true;
+    });
+
     return ChangeNotifierProvider<HomeModel>(
       create: (context) => HomeModel()
         ..shakeGesture(
@@ -84,94 +100,134 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: CircularProgressIndicator(),
                   )
                 : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: _journals.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(
-                                top: 15,
-                                left: 15,
-                                right: 15,
-                                bottom: 7.5,
-                              ),
-                              child: GestureDetector(
-                                onTap: () {
-                                  TextToSpeech.speak(
-                                      _journals[index]["description"]);
-                                },
-                                child: Card(
-                                  elevation: 5,
-                                  child: ListTile(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    leading: const Icon(
-                                      Icons.volume_up,
-                                    ),
-                                    title: Text(
-                                      _journals[index]["title"],
-                                      style: const TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.w900,
-                                      ),
-                                    ),
-                                    tileColor: Colors.white,
-                                    // Theme.of(context).colorScheme.secondary,
-                                    trailing: SizedBox(
-                                      // width:100になるように iPhone14 Pro MAX width:430/3.4
-                                      width: deviceWidth / 3.9,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        children: [
-                                          IconButton(
-                                            icon: const Icon(Icons.edit),
-                                            onPressed: () {
-                                              showModalBottomSheet(
-                                                context: context,
-                                                elevation: 20,
-                                                isScrollControlled: true,
-                                                builder: (context) {
-                                                  return EditDialog(
-                                                    id: _journals[index]['id'],
-                                                    category: category,
-                                                    journals: _journals,
-                                                    refreshJournals:
-                                                        refreshJournals,
+                      SizedBox(
+                        height: deviceHeight * 0.87,
+                        child: Stack(
+                          children: [
+                            ListView.builder(
+                              itemCount: _journals.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 15,
+                                    left: 15,
+                                    right: 15,
+                                    bottom: 7.5,
+                                  ),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      TextToSpeech.speak(
+                                          _journals[index]["description"]);
+                                    },
+                                    child: Card(
+                                      elevation: 5,
+                                      child: ListTile(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        leading: const Icon(
+                                          Icons.volume_up,
+                                        ),
+                                        title: Text(
+                                          _journals[index]["title"],
+                                          style: const TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.w900,
+                                          ),
+                                        ),
+                                        tileColor: Colors.white,
+                                        // Theme.of(context).colorScheme.secondary,
+                                        trailing: SizedBox(
+                                          // width:100になるように iPhone14 Pro MAX width:430/3.4
+                                          width: deviceWidth / 3.9,
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              IconButton(
+                                                icon: const Icon(Icons.edit),
+                                                onPressed: () {
+                                                  showModalBottomSheet(
+                                                    context: context,
+                                                    elevation: 20,
+                                                    isScrollControlled: true,
+                                                    builder: (context) {
+                                                      return EditDialog(
+                                                        id: _journals[index]
+                                                            ['id'],
+                                                        category: category,
+                                                        journals: _journals,
+                                                        refreshJournals:
+                                                            refreshJournals,
+                                                      );
+                                                    },
                                                   );
                                                 },
-                                              );
-                                            },
-                                          ),
-                                          IconButton(
-                                            icon: const Icon(Icons.delete),
-                                            onPressed: () {
-                                              showDialog(
-                                                context: context,
-                                                builder: (_) {
-                                                  return DeleteDialog(
-                                                    title: _journals[index]
-                                                        ["title"],
-                                                    index: index,
-                                                    journals: _journals,
-                                                    refreshJournals:
-                                                        refreshJournals,
+                                              ),
+                                              IconButton(
+                                                icon: const Icon(Icons.delete),
+                                                onPressed: () {
+                                                  showDialog(
+                                                    context: context,
+                                                    builder: (_) {
+                                                      return DeleteDialog(
+                                                        title: _journals[index]
+                                                            ["title"],
+                                                        index: index,
+                                                        journals: _journals,
+                                                        refreshJournals:
+                                                            refreshJournals,
+                                                      );
+                                                    },
                                                   );
                                                 },
-                                              );
-                                            },
+                                              ),
+                                            ],
                                           ),
-                                        ],
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
+                                );
+                              },
+                            ),
+                            GestureDetector(
+                              dragStartBehavior: DragStartBehavior.down,
+                              onPanUpdate: ((details) {
+                                position = details.localPosition;
+                                setState(() {});
+                              }),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    left: position.dx,
+                                    top: position.dy,
+                                    child: FloatingActionButton(
+                                      heroTag: "add",
+                                      child: const Icon(Icons.add),
+                                      onPressed: () {
+                                        showModalBottomSheet(
+                                          context: context,
+                                          elevation: 20,
+                                          isScrollControlled: true,
+                                          builder: (context) {
+                                            return EditDialog(
+                                              id: null,
+                                              category: category,
+                                              journals: _journals,
+                                              refreshJournals: refreshJournals,
+                                            );
+                                          },
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
                               ),
-                            );
-                          },
+                            ),
+                          ],
                         ),
                       ),
                       Container(
@@ -215,36 +271,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ],
                   ),
-            floatingActionButton: Padding(
-              padding: EdgeInsets.only(
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: deviceHeight * 0.16,
-              ),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                      context: context,
-                      elevation: 20,
-                      isScrollControlled: true,
-                      builder: (context) {
-                        return EditDialog(
-                          id: null,
-                          category: category,
-                          journals: _journals,
-                          refreshJournals: refreshJournals,
-                        );
-                      },
-                    );
-                  },
-                  heroTag: "add",
-                  child: const Icon(Icons.add),
-                ),
-              ),
-            ),
           );
         },
       ),
